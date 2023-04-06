715218648d87b114d2e2df243086f44e62a3b5b4
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 19:16:46 -0700

    Created isWinner props value as boolean and destructured it in component

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -4,16 +4,18 @@ import React from 'react'
 import Pokecard from '../pokecard/Pokecard';
 const Pokedex = (props) => {
   // Destructuring props
-  const { pokemon, exp } = props;
+  const { pokemon, exp, isWinner } = props;
   // Looping through pokeman and createing pokecard component to display on dom
   const displayPokemon = pokemon.map((item, i) => (
   <Pokecard name={item.name} type={item.type} exp={item.base_experience} key={i} />
   ));
+  // Creating message to display whether winner or loser
+  let msg;
   return (
     <div className='pokedex'>
       <h2 className='pokedex__title'>Pokedex</h2>
       <p>Total Experience: {exp}</p>
-      <p></p>
+      <p>{msg}</p>
       <div className='pokedex-cards'>
         {displayPokemon}
       </div>
