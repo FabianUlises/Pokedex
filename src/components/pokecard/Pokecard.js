c459b69ed6fbb9736be71355d838041a826c27f2
FabianUlises <ulisesfab.car@outlook.com>
2023-04-05 18:36:19 -0700

    Created pokecard component and added dummy  content

---
+++ src/components/pokecard/Pokecard.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from 'react';
+
+const Pokecard = () => {
+  return (
+    <div className='pokecard'>
+        <h2>name</h2>
+        <h5>Type: </h5>
+        <h5>Exp: </h5>
+    </div>
+  );
+};
+
+export default Pokecard;
