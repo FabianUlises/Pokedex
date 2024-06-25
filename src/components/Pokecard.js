cfd942961a986a62fa532247fe55907edef117f0
fabian ulises <ulises.fab@outlook.com>
2024-06-24 23:54:51 -0700

    Updated pokecard component. Added pokemon img using pokemon id.

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 const Pokecard = (props) => {
     // URL for pokemon img
-    const pokeApiURL= `https://assets.pokemon.com/assets/cms2/img/pokemon/detail/`;
+    const pokeApiURL= `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
     // If id is less than 3 digits appending two 0 to the start. API img must be in 3 digits
     let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
     const imgSrc = `${pokeApiURL}${padToThree(props.id)}.png`;
+    console.log(imgSrc)
     return(
-        <div>
+        <div key={props.id}>
+            <img src={imgSrc} alt={props.name} />
             <p>{props.exp}</p>
             <h4>{props.name}</h4>
             <p>{props.type}</p>
