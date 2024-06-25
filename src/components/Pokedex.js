5c2264707b7bf5ecb11cf511f4c904ab756bb91c
FabianUlises <ulisesfab.car@outlook.com>
2024-06-25 15:24:49 -0700

    Updated pokedex component. Added a container for content to control layout.

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,9 +4,11 @@ const Pokedex = (props) => {
     return (
         <div className="pokedex-container">
             <h2 className="pokedex-title">Pokedex</h2>
-            {props.pokemon.map((p) => ( 
-                <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-            ))}
+            <div className="pokedex-content">
+                {props.pokemon.map((p) => ( 
+                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+                ))}
+            </div>
         </div>
     );
 }
