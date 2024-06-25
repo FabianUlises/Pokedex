a32ada09cabed93cc512aa794cba1a7bc07e06a7
fabian ulises <ulises.fab@outlook.com>
2024-06-24 23:49:53 -0700

    Updated pokecard component. Added dynamic url depending on pokemon id to fetch pokemon img.

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 const Pokecard = (props) => {
+    // URL for pokemon img
+    const pokeApiURL= `https://assets.pokemon.com/assets/cms2/img/pokemon/detail/`;
+    // If id is less than 3 digits appending two 0 to the start. API img must be in 3 digits
+    let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+    const imgSrc = `${pokeApiURL}${padToThree(props.id)}.png`;
     return(
         <div>
             <p>{props.exp}</p>
