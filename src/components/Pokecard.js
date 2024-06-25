00a32dad2bcd287bb075178daa5e7c7c0fea1b8a
fabian ulises <ulises.fab@outlook.com>
2024-06-24 23:10:15 -0700

    Created a pokecard component & rendering it in pokegame component.

---
+++ src/components/Pokecard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+const Pokecard = () => {
+    return(
+        <div>
+            <h4>Pokecard</h4>
+        </div>
+    )
+}
+export default Pokecard;
--- src/components/Pokegame.js
+++ src/components/Pokegame.js
@@ -1,7 +1,9 @@
 import React, { Components } from 'react';
+import Pokecard from './Pokecard';
 const Pokegame = () => (
     <div className='pokegame-container'>
         <h1>Pokegame</h1>
+        <Pokecard />
     </div>
 )
 export default Pokegame;
